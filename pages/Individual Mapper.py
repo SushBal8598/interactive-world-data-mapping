8db9395e8b8cbe84620da06b242b91e40446c12c
Sushanth Balaraman <109186110+SushBal8598@users.noreply.github.com>
2024-11-28 06:59:53 +0000

    Finished converting architecutre; everything instanced at beginning

--- pages/Individual Mapper.py
+++ pages/Individual Mapper.py
@@ -255,19 +255,9 @@ if option != 'Placeholder':
 
     with col1:
 
-        spreadsheet = client.open_by_key(st.secrets["google_sheets"]["spreadsheet_id"])
-        pop_sheet = spreadsheet.worksheet('TotPop')
-        pop_data = pop_sheet.get_all_values()
 
-        pop_df = pd.DataFrame(pop_data, columns = ['Country Name','Country Code','Indicator Name','Indicator Code',	'1960',
-    '1961',	'1962','1963',	'1964', '1965','1966','1967','1968','1969','1970','1971','1972','1973','1974',
-    '1975',	'1976',	'1977','1978','1979','1980','1981','1982','1983','1984','1985', '1986',	'1987',	'1988',	
-    '1989',	'1990',	'1991',	'1992',	'1993',	'1994',	'1995',	'1996',	'1997',	'1998',	'1999',	'2000',	'2001',	'2002',	
-    '2003',	'2004',	'2005',	'2006',	'2007',	'2008',	'2009',	'2010',	'2011',	'2012',	'2013',	'2014',	'2015',	'2016',	
-    '2017',	'2018','2019','2020','2021','2022','2023'])
-
-        country_df = pop_df[pop_df['Country Name'] == option]
-        country_df_index = int((pop_df[pop_df['Country Name'] == option].index)[0])
+        country_df = global_variables.pop_df[global_variables.pop_df['Country Name'] == option]
+        country_df_index = int((global_variables.pop_df[global_variables.pop_df['Country Name'] == option].index)[0])
         population = format_number(int(country_df.at[country_df_index, (my_year)]))
         
         population_safe = html.escape(str(population))
@@ -286,12 +276,9 @@ if option != 'Placeholder':
         st.markdown(population_html, unsafe_allow_html=True)
     
     with col2:
-        spreadsheet = client.open_by_key(st.secrets["google_sheets"]["spreadsheet_id"])
-        gdp_sheet = spreadsheet.worksheet('GDP_Data')
-        gdp_data = gdp_sheet.get_all_values()
 
-        my_data = make_general_sheet(gdp_data, 'gdp_df')[make_general_sheet(gdp_data, 'gdp_df')['Country Name'] == option]
-        gdp_df_index = int(make_general_sheet(gdp_data, 'gdp_df')[make_general_sheet(gdp_data, 'gdp_df')['Country Name'] == option].index[0])
+        my_data = make_general_sheet(global_variables.gdp_data, 'gdp_df')[make_general_sheet(global_variables.gdp_data, 'gdp_df')['Country Name'] == option]
+        gdp_df_index = int(make_general_sheet(global_variables.gdp_data, 'gdp_df')[make_general_sheet(global_variables.gdp_data, 'gdp_df')['Country Name'] == option].index[0])
         try:
             gdp = format_number(int(my_data.at[gdp_df_index, (my_year)]))
         except:
