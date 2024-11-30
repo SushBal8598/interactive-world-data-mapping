88bd1ab98a9ade8d549adff4ef03e1ee38d2e0ad
Sushanth Balaraman <109186110+SushBal8598@users.noreply.github.com>
2024-11-30 06:59:15 +0000

    global indicator groupings

--- pages/Altair Graphing.py
+++ pages/Altair Graphing.py
@@ -3,16 +3,22 @@ import altair as alt
 import global_variables
 
 try:
-        st.title(st.session_state.option)
+        indiv_title = (f"<h1 style='text-align: center; color: black;margin-bottom: -50px;'>Individual Visualization:</h1>")
+        st.html(indiv_title)
+
+        named_title = (f"<h1 style='text-align: center; color: black; text-decoration: underline;'>{st.session_state.option}</h1>")
+        st.html(named_title)
 except:
         st.info('Navigate to Individual Mapper first.')
 
 col1,col2 = st.columns(2)
 
 with col1:
+        st.html("<h6 style='text-align: center; color: black;margin-bottom: -25px;'>Step 1: Input Query</h6>")
+
         option_mapping = st.selectbox(
         "Select a pre-loaded statistics set, or continue with custom.",
-        ("Poverty", "Custom"),
+        ("Poverty", "Education","Agriculture","Custom"),
 )
         
         if option_mapping == 'Custom':
@@ -26,5 +32,7 @@ with col1:
         ("Scatterplot", "Lineplot", 'Boxplot', 'Bubble Chart'),
 )
 
+with col2:
+        st.html("<h6 style='text-align: center; color: black;margin-bottom: -25px;'>Step 2: Validate Request</h6>")
 #test global stuff      
-st.write(global_variables.df) #successful; should be able to initialize as necessary
+#st.write(global_variables.df) #successful; should be able to initialize as necessary
